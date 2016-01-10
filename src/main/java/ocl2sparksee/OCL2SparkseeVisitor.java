ccca95904529bab006902f4f54863524969dfe44
rpau <rpau@ac.upc.edu>
2016-01-10 19:27:35 +0100

    first version

---
+++ src/main/java/ocl2sparksee/OCL2SparkseeVisitor.java
@@ -0,0 +1,224 @@
+package ocl2sparksee;
+
+import com.sparsity.ocl.ast.BooleanLiteralExp;
+import com.sparsity.ocl.ast.CallExp;
+import com.sparsity.ocl.ast.ConnectableElement;
+import com.sparsity.ocl.ast.Constraint;
+import com.sparsity.ocl.ast.Expression;
+import com.sparsity.ocl.ast.ExpressionInOcl;
+import com.sparsity.ocl.ast.FeatureCallExp;
+import com.sparsity.ocl.ast.IfExp;
+import com.sparsity.ocl.ast.IntegerLiteralExp;
+import com.sparsity.ocl.ast.IterateExp;
+import com.sparsity.ocl.ast.IteratorExp;
+import com.sparsity.ocl.ast.LetExpression;
+import com.sparsity.ocl.ast.LiteralExp;
+import com.sparsity.ocl.ast.LoopExp;
+import com.sparsity.ocl.ast.NamedElement;
+import com.sparsity.ocl.ast.NavigationCallExp;
+import com.sparsity.ocl.ast.NumericLiteralExp;
+import com.sparsity.ocl.ast.OclAstVisitor;
+import com.sparsity.ocl.ast.OclExpression;
+import com.sparsity.ocl.ast.Operation;
+import com.sparsity.ocl.ast.OperationCallExp;
+import com.sparsity.ocl.ast.Parameter;
+import com.sparsity.ocl.ast.PropertyCallExp;
+import com.sparsity.ocl.ast.RealLiteralExp;
+import com.sparsity.ocl.ast.StateExp;
+import com.sparsity.ocl.ast.StringLiteralExp;
+import com.sparsity.ocl.ast.TupleLiteralExp;
+import com.sparsity.ocl.ast.Type;
+import com.sparsity.ocl.ast.TypeExp;
+import com.sparsity.ocl.ast.TypedElement;
+import com.sparsity.ocl.ast.Variable;
+import com.sparsity.ocl.ast.VariableExp;
+
+public class OCL2SparkseeVisitor implements OclAstVisitor{
+
+	@Override
+	public void visit(BooleanLiteralExp booleanLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(CallExp callExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(ConnectableElement connectableElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Constraint constraint) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Expression expression) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(ExpressionInOcl expressionInOcl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(FeatureCallExp featureCallExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(IfExp ifExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(IntegerLiteralExp integerLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(IterateExp iterateExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(IteratorExp iteratorExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(LetExpression letExpression) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(LiteralExp literalExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(LoopExp loopExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(NamedElement namedElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(NavigationCallExp navigationCallExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(NumericLiteralExp numericLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(OclExpression oclExpression) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Operation operation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(OperationCallExp operationCallExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Parameter parameter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(PropertyCallExp propertyCallExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(RealLiteralExp realLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(StateExp stateExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(StringLiteralExp stringLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(TupleLiteralExp tupleLiteralExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Type type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(TypedElement typedElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(TypeExp typedExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Variable variable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(VariableExp variableExp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
